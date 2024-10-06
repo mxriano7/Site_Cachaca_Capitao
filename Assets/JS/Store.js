eee71dd6f82b59df9fb592c1b55db3c68bfac598
Paulo Cesar <you@example.com>
2024-10-06 13:52:30 -0300

    Adicionando arquivos

--- Assets/JS/Store.js
+++ Assets/JS/Store.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 cartSection.style.display = 'none';
             }, 300);
         } else {
-            cartSection.style.display = 'block';
+            cartSection.style.display = '';
             setTimeout(() => {
                 cartSection.classList.add('show');
             }, 1);
