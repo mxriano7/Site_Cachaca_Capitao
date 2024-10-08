82d0beb1e4e10ade9a6fce6e0b656c779084108b
Paulo Cesar <you@example.com>
2024-10-07 23:09:47 -0300

    Adicionando arquivos

--- Assets/JS/Store.js
+++ Assets/JS/Store.js
@@ -637,7 +637,7 @@ function displayShippingOptions(options) {
             const optionElement = document.createElement('div');
             optionElement.classList.add('shipping-option');
             optionElement.innerHTML = `
-                <label>Opção de Frete:
+                <label>Opção de Frete:<br>
                     <input type="radio" name="shippingOption" value="${price}" data-service="${option.name}">
                     ${option.name}(Correios) - R$ ${price.toFixed(2)}<br>
                     Prazo para entrega: ${option.delivery_time} dias
